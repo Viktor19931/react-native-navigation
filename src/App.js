fb9dd528db2373194937d79857b2f66a67d8e96b
Viktor Hordubey <viktor_hordubey@yahoo.com>
2018-06-30 20:34:57 +0300

    degrade firebase to 5.0.2 version

--- src/App.js
+++ src/App.js
@@ -1,55 +1,41 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-const firebase = require('firebase');
+import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk'
 
+import { Header } from './components/common'
 import reducers from './reducers'
 import LoginForm from "./components/LoginForm";
 
 export default class App extends Component{
-  componentWillMount() {
-    this.initFirebase();
-  }
+    componentWillMount() {
+        this.initFirebase();
+    }
 
-  initFirebase() {
-      const config = {
-          apiKey: "AIzaSyBji7T71j-E3grXhFRQlGQnz2eSU6EF3aE",
-          authDomain: "auth-react-cbf3e.firebaseapp.com",
-          databaseURL: "https://auth-react-cbf3e.firebaseio.com",
-          projectId: "auth-react-cbf3e",
-          storageBucket: "auth-react-cbf3e.appspot.com",
-          messagingSenderId: "449649701681"
-      };
-      firebase.initializeApp(config);
-  }
+    initFirebase() {
+        const config = {
+            apiKey: "AIzaSyBji7T71j-E3grXhFRQlGQnz2eSU6EF3aE",
+            authDomain: "auth-react-cbf3e.firebaseapp.com",
+            databaseURL: "https://auth-react-cbf3e.firebaseio.com",
+            projectId: "auth-react-cbf3e",
+            storageBucket: "auth-react-cbf3e.appspot.com",
+            messagingSenderId: "449649701681"
+        };
+        firebase.initializeApp(config);
+    }
 
-  render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
-    return (
-      <Provider store={store}>
-          <LoginForm />
-      </Provider>
-    );
-  }
+        return (
+            <Provider store={store}>
+               <View>
+                   <Header headerText={'Authentication'}/>
+                   <LoginForm />
+               </View>
+            </Provider>
+        );
+    }
 }
-
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-};
